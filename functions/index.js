f9ee4b80a20056fb95c97ae8b1a3b9fd2b485edd
iotmani <salism3@gmail.com>
2018-08-20 18:24:11 -0400

    Minor spelling tense correction
    See
    https://www.quora.com/Which-is-more-grammatically-correct-was-run-or-was-ran

--- functions/index.js
+++ functions/index.js
@@ -16,5 +16,5 @@ var functions = require('firebase-functions');
 
 exports.hourly_job =
   functions.pubsub.topic('hourly-tick').onPublish((event) => {
-    console.log("This job is ran every hour!")
+    console.log("This job is run every hour!")
   });
